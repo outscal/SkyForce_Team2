504ce63ec7326b941f01fcc007aa937d1dc844bf
Sethunath Raveendran <sethunath3@gmail.com>
2019-09-28 00:26:52 +0530

    Creating player controller

---
+++ Assets/Scripts/FighterJet/FighterJetController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterJetController
+{
+    private FighterJetModel model;
+    private FighterJetView view;
+    public FighterJetController()
+    {
+        model = new FighterJetModel();
+        view = GameObject.Instantiate<FighterJetView>();
+    }
+}
