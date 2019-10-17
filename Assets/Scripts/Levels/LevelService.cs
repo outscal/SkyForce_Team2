911a1f5bbe28051b377da2848bb4fe406ddda7a8
Sethunath Raveendran <sethunath3@gmail.com>
2019-10-17 15:47:33 +0530

    Saving states

---
+++ Assets/Scripts/Levels/LevelService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SkyForce.Generics;
+
+namespace SkyForce.Level
+{
+    public class LevelService : GenericMonoSingleton<LevelService>
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
